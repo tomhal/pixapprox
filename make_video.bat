a2d5b4725e5862f0dc51ff6aab656959ad204f93
Tommy Hallgren <thallgren@gmail.com>
2023-10-16 09:06:22 +0200

    Update make_video.bat

--- make_video.bat
+++ make_video.bat
@@ -1,1 +1,3 @@
-ffmpeg.exe -framerate 15 -i result/%05d.png -s:v 512x256 -c:v libx264 -profile:v high -crf 12 -pix_fmt yuv420p movie.mp4
+rem ffmpeg.exe -f image2 -framerate 15 -i %05d.png -s:v 512x256 -c:v libx264 -profile:v high -crf 12 -pix_fmt yuv420p movie.mp4
+
+mencoder mf://*.png -mf w=800:h=600:fps=25:type=png -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -oac copy -o output.avi
